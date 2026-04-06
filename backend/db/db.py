3a815dd571e7deda26790e6cfe201ea21413a08e
Michael <mr.michael.antony@gmail.com>
2026-04-06 14:17:59 +0530

    SQL server connection string update

--- backend/db/db.py
+++ backend/db/db.py
@@ -2,20 +2,38 @@
 
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
+from urllib.parse import quote_plus
 import os
 class SQLiteDB:
     def __init__(self):
         environment_flag = os.getenv('environment_flag')
-        server = os.getenv('SQL_SERVER', 'localhost')  # e.g., localhost\SQLEXPRESS
-        database = os.getenv('SQL_DATABASE', 'actual-result-prod')
-        username = os.getenv('SQL_USER', 'sa')
-        password = os.getenv('SQL_PASSWORD', 'YourStrong!Passw0rd')
-        driver = 'ODBC Driver 17 for SQL Server'
+        if environment_flag == 'local':
+            server = 'localhost'
+            database = 'actual-result-prod'
+            driver = 'ODBC Driver 17 for SQL Server'
 
-        # # SQL Server connection string
-        # db_url = f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server"
-        # Windows Authentication
-        db_url = f"mssql+pyodbc://@{server}/{database}?driver={driver}&trusted_connection=yes"
+            # Windows Authentication
+            db_url = f"mssql+pyodbc://@{server}/{database}?driver={driver}&trusted_connection=yes"
+        else:
+            server = os.getenv('SQL_SERVER', 'localhost')  # e.g., localhost\SQLEXPRESS
+            database = os.getenv('SQL_DATABASE', 'actual-result-prod')
+            username = os.getenv('SQL_USER', 'sa')
+            password = os.getenv('SQL_PASSWORD', 'YourStrong!Passw0rd')
+            driver = os.getenv('DRIVER','ODBC Driver 17 for SQL Server')
+
+            # # SQL Server connection string
+            params = quote_plus(
+                f"DRIVER={{{driver}}};"
+                f"SERVER={server};"
+                f"DATABASE={database};"
+                f"UID={username};"
+                f"PWD={password};"
+                "TrustServerCertificate=yes;"
+            )
+
+            db_url = f"mssql+pyodbc:///?odbc_connect={params}"
+            # db_url = f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server"
+
 
         print(f"Connecting to SQL Server at {server}, database {database}")
 
